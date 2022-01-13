f086f4d0480f46618494fdb821d9e9bb247a1890
gusujin <gusu@likelion.net>
2022-01-13 12:28:25 +0900

    ✨NEW: add redux-persist

--- src/TodoList.js
+++ src/TodoList.js
@@ -14,82 +14,87 @@ const TodoList = ({ list, onDelete, onComplete }) => {
   return (
     <div>
       <ul style={{ listStyle: "none", paddingLeft: 0 }}>
-        {list && list.map((todoitem, index) => {
-          return (
-            <li
-              key={index}
-              style={{
-                fontSize: "18px",
-                display: "flex",
-                alignItems: "center",
-                borderBottom: "1px solid #aaa",
-                boxSizing: "border-box",
-                padding: "0 15px",
-              }}
-            >
-              <span
-                style={{ marginRight: "10px", cursor: "pointer" }}
-                onClick={() => onComplete(todoitem.id)}
-              >
-                {todoitem.is_completed ? (
-                  <MdCheckBox />
-                ) : (
-                  <MdCheckBoxOutlineBlank />
-                )}
-              </span>
-              <p
+        {list &&
+          list.map((todoitem) => {
+            return (
+              <li
+                key={todoitem.id}
                 style={{
-                  flex: 1,
-                  textDecoration: todoitem.is_completed
-                    ? "line-through"
-                    : "none",
+                  fontSize: "18px",
+                  display: "flex",
+                  alignItems: "center",
+                  borderBottom: "1px solid #aaa",
+                  boxSizing: "border-box",
+                  padding: "0 15px",
                 }}
               >
-                {todoitem.isEditing ? (
-                  <input
-                    value={editText}
-                    onChange={(e) => setEditText(e.target.value)}
-                    onKeyPress={(e) =>
-                      e.key === "Enter"
-                        ? dispatch(
-                            edittodo({ id: todoitem.id, title: editText })
-                          )
-                        : null
-                    }
-                  /> //업데이트 된 데이터를 바꿔줌
-                ) : (
-                  todoitem.title // 기존 todo value
-                )}
-              </p>
-              <span
-                onClick={() => onDelete(todoitem.id)}
-                style={{ display: "inline-block", marginRight: "10px" }}
-              >
-                <MdRemoveCircleOutline
+                <span
+                  style={{ marginRight: "10px", cursor: "pointer" }}
+                  onClick={() => onComplete(todoitem.id)}
+                >
+                  {todoitem.is_completed ? (
+                    <MdCheckBox />
+                  ) : (
+                    <MdCheckBoxOutlineBlank />
+                  )}
+                </span>
+                <p
                   style={{
-                    fontSize: "22px",
-                    color: "red",
-                    cursor: "pointer",
+                    flex: 1,
+                    textDecoration: todoitem.is_completed
+                      ? "line-through"
+                      : "none",
                   }}
-                />
-              </span>
-              <span
-                onClick={() => {
-                  if (!todoitem.isEditing) {
-                    dispatch(setEdit({ id: todoitem.id }));
-                    setEditText(todoitem.title);
-                  } else {
-                    // edit 기능 dispatch
-                    dispatch(edittodo({ id: todoitem.id, title: editText }));
-                  }
-                }}
-                style={{ cursor: "pointer" }}
-              >
-                {todoitem.isEditing ? <button>수정완료</button> : <MdCreate />}
-              </span>
-            </li>
-          );
-        })}
+                >
+                  {todoitem.isEditing ? (
+                    <input
+                      value={editText}
+                      onChange={(e) => setEditText(e.target.value)}
+                      onKeyPress={(e) =>
+                        e.key === "Enter"
+                          ? dispatch(
+                              edittodo({ id: todoitem.id, title: editText })
+                            )
+                          : null
+                      }
+                    /> //업데이트 된 데이터를 바꿔줌
+                  ) : (
+                    todoitem.title // 기존 todo value
+                  )}
+                </p>
+                <span
+                  onClick={() => onDelete(todoitem.id)}
+                  style={{ display: "inline-block", marginRight: "10px" }}
+                >
+                  <MdRemoveCircleOutline
+                    style={{
+                      fontSize: "22px",
+                      color: "red",
+                      cursor: "pointer",
+                    }}
+                  />
+                </span>
+                <span
+                  onClick={() => {
+                    if (!todoitem.isEditing) {
+                      dispatch(setEdit({ id: todoitem.id }));
+                      setEditText(todoitem.title);
+                    } else {
+                      // edit 기능 dispatch
+                      dispatch(edittodo({ id: todoitem.id, title: editText }));
+                    }
+                  }}
+                  style={{ cursor: "pointer" }}
+                >
+                  {todoitem.isEditing ? (
+                    <button>수정완료</button>
+                  ) : (
+                    <MdCreate />
+                  )}
+                </span>
+              </li>
+            );
+          })}
       </ul>
     </div>
   );
--- src/app/store.js
+++ src/app/store.js
@@ -1,16 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
-import todoSlice from "../features/todo/todoSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import todoReducer from "../features/todo/todoSlice";
 
-// export const store = createStore(todoSlice, applyMiddleware(logger));
-export const store = configureStore({
-  reducer: {  todo: todoSlice,},
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const rootReducer = combineReducers({
+  todo: todoReducer,
+  //WHY? reducer name : 내가 지정한 리듀서 ,, ,를 해줘야 하는지?
+  // 꼭 toolkit에서 사용한 name을,, 가져다 써야하는지?
 });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// export const store = configureStore({
-//   reducer: todoSlice,
-//   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-//     serializableCheck: false,
-//   }).concat(logger),
-// });
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [logger],
+});
